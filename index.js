b4667fcade36132eceaab2789425cdde1e1d5e6b
VICTORRFCHIOZZINI <127430122+VICTORRFCHIOZZINI@users.noreply.github.com>
2023-04-13 13:54:08 -0300

    AULA 13/4/2023

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+const ENTRADA = require('prompt-sync')();
 console.log('OI');
+let NOME = ENTRADA('DIGITE O NOME - ');
+console.log('OI ' + NOME);
+let IDADETXT = ENTRADA(NOME + 'QUAL SUA IDADE ?')
+let IDADE = parseInt(IDADETXT);
+let RESPOSTA = 'N';
+if (IDADE < 15) {
+    RESPOSTA = ENTRADA('\n(S/N)')
+}
+else if (IDADE > 15 && IDADE < 18) {
+    RESPOSTA = ENTRADA('\n(S/N)')
+}
+else {
+    RESPOSTA = ENTRADA('\n(S/N)')
+}
+if (RESPOSTA === 'N') {
+    console.log('');
+}
+else {
+    console.log('');
+}
+console.log('');
+ENTRADA('TCHAU');
